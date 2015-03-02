9570ae245c535a4ecf13a201462bd42aa56bdcae
linx <plinux@qq.com>
2015-03-03 00:05:56 +0800

    WebGL version commit.

---
+++ WebGL/demo.js
@@ -0,0 +1,7 @@
+function main() {
+	var canvas = document.getElementById("cbox");
+	var ctx = canvas.getContext("2d");
+
+	ctx.fillStyle = "rgba(0, 0, 255, 1.0)";
+	ctx.fillRect(120, 10, 150, 150);
+}
