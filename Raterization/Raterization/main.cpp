4d6f6baa669d60d0af15bbc468b3e407c2a9cb00
linx <plinux@qq.com>
2015-03-11 22:58:02 +0800

    Rasterization ing.

---
+++ Raterization/Raterization/main.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+
+int main()
+{
+
+	return 0;
+}
+
+
+#if 0
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR szCmdLine, int iCmdShow)
+{
+	LWindow win;
+
+	win.Create(480, 480, hInstance);
+	win.MsgLoop();
+
+	return 0;
+}
+#endif
---
+++ Raterization/Raterization/stdafx.h
@@ -0,0 +1,8 @@
+#include <Windows.h>
+#include <assert.h>
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <ctime>
+
+#include "inc\win.h"
